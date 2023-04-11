80e3e4de8b3a12d57a97db3e0b91ca9ca45be7b1
Pedro Araujo <shikbane.developer@gmail.com>
2023-04-11 12:41:22 -0300

    Melhorando código

--- script.js
+++ script.js
@@ -1,10 +1,59 @@
-function carregar(){
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('foto')
-    var mes = new Date()
-    var data = mes.getMonth()
+const msg = document.querySelector(".msg");
+const img = document.querySelector("#foto");
+const mes = new Date();
+const data = mes.getMonth();
 
-    if (data >= 9 && data <= 10) {
+let sesson = "";
+
+const estaçao = {
+  Primavera: {
+    img: "img/spring.png",
+    color: "#c0958e",
+  },
+  Outono: {
+    img: "img/autum.png",
+    color: "#d94c3a",
+  },
+  Inverno: {
+    img: "img/winter.png",
+    color: "#6c7eab",
+  },
+  Verao: {
+    img: "img/summer.png",
+    color: "#579ae1",
+  },
+};
+
+data >= 9 && data <= 10
+  ? (sesson = "Primavera")
+  : data >= 3 && data <= 5
+  ? (sesson = "Outono")
+  : data >= 6 && data <= 8
+  ? (sesson = "Inverno")
+  : (sesson = "Verão");
+
+window.addEventListener("load", function () {
+  if (sesson === "Primavera") {
+    msg.textContent = "Primavera";
+    img.src = estaçao[sesson].img;
+    document.body.style.background = "#c0958e";
+  } else if (sesson === "Outono") {
+    msg.textContent = "Outono";
+    img.src = estaçao[sesson].img;
+    document.body.style.background = "#d94c3a";
+  } else if (sesson === "Inverno") {
+    msg.textContent = "Inverno";
+    img.src = estaçao[sesson].img;
+    document.body.style.background = "#6c7eab";
+  } else {
+    msg.textContent = "Verão";
+    img.src = estaçao[sesson].img;
+    document.body.style.background = "#579ae1";
+  }
+});
+
+/*
+ if (data >= 9 && data <= 10) {
         msg.innerHTML = 'Primavera'
         document.body.style.background = '#c0958e'
         img.src = 'img/spring.png'
@@ -21,4 +70,35 @@ function carregar(){
         document.body.style.background = '#579ae1'
         img.src = 'img/summer.png'
     }
-}
+
+
+
+    const seasons = {
+        primavera: {
+            color: '#c0958e',
+            image: 'img/spring.png'
+        },
+        outono: {
+            color: '#d94c3a',
+            image: 'img/autum.png'
+        },
+        inverno: {
+            color: '#6c7eab',
+            image: 'img/winter.png'
+        },
+        verao: {
+            color: '#579ae1',
+            image: 'img/summer.png'
+        }
+    };
+
+
+
+
+
+
+
+
+
+
+    */
