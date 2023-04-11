447afc045750e5020471897d2cd72d531d3d070a
Pedro Araujo <shikbane.developer@gmail.com>
2023-04-11 12:47:10 -0300

    Refatoração

--- script.js
+++ script.js
@@ -33,72 +33,8 @@ data >= 9 && data <= 10
   : (sesson = "Verão");
 
 window.addEventListener("load", function () {
-  if (sesson === "Primavera") {
-    msg.textContent = "Primavera";
-    img.src = estaçao[sesson].img;
-    document.body.style.background = "#c0958e";
-  } else if (sesson === "Outono") {
-    msg.textContent = "Outono";
-    img.src = estaçao[sesson].img;
-    document.body.style.background = "#d94c3a";
-  } else if (sesson === "Inverno") {
-    msg.textContent = "Inverno";
-    img.src = estaçao[sesson].img;
-    document.body.style.background = "#6c7eab";
-  } else {
-    msg.textContent = "Verão";
-    img.src = estaçao[sesson].img;
-    document.body.style.background = "#579ae1";
-  }
+  msg.textContent = sesson;
+  img.src = estaçao[sesson].img;
+  this.document.body.style.background = estaçao[sesson].color;
 });
 
-/*
- if (data >= 9 && data <= 10) {
-        msg.innerHTML = 'Primavera'
-        document.body.style.background = '#c0958e'
-        img.src = 'img/spring.png'
-    } else if (data >= 3 && data <= 5){
-        msg.innerHTML = 'Outono'
-        document.body.style.background = '#d94c3a'
-        img.src = 'img/autum.png'
-    } else if (data >= 6 && data <= 8){
-        msg.innerHTML = 'Inverno'
-        document.body.style.background = '#6c7eab'
-        img.src = 'img/winter.png'
-    } else {
-        msg.innerHTML = 'Verão'
-        document.body.style.background = '#579ae1'
-        img.src = 'img/summer.png'
-    }
-
-
-
-    const seasons = {
-        primavera: {
-            color: '#c0958e',
-            image: 'img/spring.png'
-        },
-        outono: {
-            color: '#d94c3a',
-            image: 'img/autum.png'
-        },
-        inverno: {
-            color: '#6c7eab',
-            image: 'img/winter.png'
-        },
-        verao: {
-            color: '#579ae1',
-            image: 'img/summer.png'
-        }
-    };
-
-
-
-
-
-
-
-
-
-
-    */
